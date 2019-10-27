33c78ec6f4552acff6accba2023f5465a3f75258
Aaron Chang <aaronchang153@gmail.com>
2019-10-26 20:32:05 -0700

    Test file io and TextView change

--- app/src/main/java/com/g_house/g_house_app/MainActivity.java
+++ app/src/main/java/com/g_house/g_house_app/MainActivity.java
@@ -1,7 +1,14 @@
 package com.g_house.g_house_app;
 
+import android.content.Context;
+import android.icu.util.Output;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try
+        {
+            OutputStreamWriter osw = new OutputStreamWriter((getApplicationContext().openFileOutput("test.txt", Context.MODE_PRIVATE)));
+            osw.write("Test 123 456 789");
+            osw.close();
+
+            char[] buffer = new char[100];
+            InputStreamReader isw = new InputStreamReader(getApplicationContext().openFileInput("test.txt"));
+            isw.read(buffer, 0, 100);
+            TextView t = (TextView) findViewById(R.id.centered_text);
+            t.setText(buffer, 0, 100);
+            isw.close();
+        }
+        catch(Exception ignore) {}
     }
 }
