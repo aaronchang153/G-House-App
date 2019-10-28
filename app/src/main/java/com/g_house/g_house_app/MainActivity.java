11cc46a2e90a479ffa0f775460d381b983d6a0ff
Aaron Chang <aaronchang153@gmail.com>
2019-10-27 19:08:40 -0700

    Added some more Bluetooth features

--- app/src/main/java/com/g_house/g_house_app/MainActivity.java
+++ app/src/main/java/com/g_house/g_house_app/MainActivity.java
@@ -2,14 +2,19 @@ package com.g_house.g_house_app;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     BluetoothAdapter bluetoothAdapter;
@@ -38,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         //catch(Exception ignore) {}
     }
 
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
         if(requestCode == REQUEST_ENABLE_BT && resultCode == RESULT_OK)
@@ -87,5 +93,76 @@ public class MainActivity extends AppCompatActivity {
         }
 
         t.append("Done.\n");
+    }
+
+    private class ConnectThread extends Thread
+    {
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+
+        ConnectThread(BluetoothDevice device)
+        {
+            BluetoothSocket tmp = null;
+            mmDevice = device;
+            try
+            {
+                tmp = device.createRfcommSocketToServiceRecord(UUID.randomUUID());
+            }
+            catch(Exception ignored) {}
+
+            mmSocket = tmp;
+        }
+
+        @Override
+        public void run()
+        {
+            bluetoothAdapter.cancelDiscovery();
+            try
+            {
+                mmSocket.connect();
+            }
+            catch(IOException connectException)
+            {
+                try
+                {
+                    mmSocket.close();
+                }
+                catch (IOException ignored) { }
+
+                return;
+            }
+
+            // Do whatever needs to be done with mmSocket
+            manageSocket();
+        }
+
+        public void cancel()
+        {
+            try
+            {
+                mmSocket.close();
+            }
+            catch (IOException ignored) { }
+        }
+
+        public void manageSocket()
+        {
+            InputStream inputStream;
+            OutputStream outputStream;
+
+            try
+            {
+                inputStream = mmSocket.getInputStream();
+                outputStream = mmSocket.getOutputStream();
+
+                byte[] buffer = new byte[1024];
+
+                inputStream.read(buffer);
+                outputStream.write(buffer);
+            }
+            catch(Exception ignored) {}
+
+            cancel();
+        }
     }
 }
