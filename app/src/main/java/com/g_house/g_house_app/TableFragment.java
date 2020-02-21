e590d4739f7c837e3e7d135ad61194cf899bf1e6
Aaron Chang <aaronchang153@gmail.com>
2020-02-20 20:17:06 -0800

    Bug fix. Successfully get data.log and update table

--- app/src/main/java/com/g_house/g_house_app/TableFragment.java
+++ app/src/main/java/com/g_house/g_house_app/TableFragment.java
@@ -22,7 +22,9 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.Set;
 import java.util.UUID;
 
@@ -51,12 +53,12 @@ public class TableFragment extends Fragment {
         piDevice = null;
 
         log = inflater.inflate(R.layout.fragment_log, container, false).findViewById(R.id.log_text);
+        //log = view.findViewById(R.id.debug);
+        log.append("Debug log start\n");
 
         final Button refreshB = view.findViewById(R.id.refreshButton);
         refreshB.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                String[] asdf = {"asdf", "qwer", "zxcv", "tyui"};
-                addTableRow((TableLayout)view.findViewById(R.id.dataTable), asdf);
                 if(piDevice != null)
                 {
                     ConnectThread thread = new ConnectThread(piDevice);
@@ -159,7 +161,7 @@ public class TableFragment extends Fragment {
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if(bluetoothAdapter == null)
         {
-            log.append("Device doesn'log support bluetooth.\n");
+            log.append("Device doesn't support bluetooth.\n");
             return;
         }
 
@@ -214,109 +216,134 @@ public class TableFragment extends Fragment {
         for(String tok : data)
         {
             TextView col = new TextView(getActivity());
-            col.setText(tok);
+            col.setText(tok.substring(0, Math.min(tok.length(), 5))); //TODO: Fix table formatting in a better way
             row.addView(col);
         }
 
         tl.addView(row);
     }
 
-private class ConnectThread extends Thread
-{
-    private final BluetoothSocket mmSocket;
-    private final BluetoothDevice mmDevice;
-
-    ConnectThread(BluetoothDevice device)
+    private class ConnectThread extends Thread
     {
-        BluetoothSocket tmp = null;
-        mmDevice = device;
-        try
-        {
-            //Using well-known SPP UUID
-            tmp = mmDevice.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
-        }
-        catch(Exception ignored) {}
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
 
-        mmSocket = tmp;
-    }
-
-    @Override
-    public void run()
-    {
-        bluetoothAdapter.cancelDiscovery();
-        try
+        ConnectThread(BluetoothDevice device)
         {
-            log.append("Attempting to connect to Pi.\n");
-
-            mmSocket.connect();
-        }
-        catch(IOException connectException)
-        {
-            log.append("Failed to connect to Pi: ");
-            log.append(String.format("%s\n", connectException.toString()));
+            BluetoothSocket tmp = null;
+            mmDevice = device;
             try
             {
-                mmSocket.close();
+                //Using well-known SPP UUID
+                tmp = mmDevice.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
             }
-            catch (IOException ignored) { }
+            catch(Exception ignored) {}
 
-            return;
+            mmSocket = tmp;
         }
 
-        // Do whatever needs to be done with mmSocket
-        manageSocket();
-    }
-
-    public void cancel() {
-        try {
-            mmSocket.close();
-        } catch (IOException ignored) {
-        }
-    }
-
-    public void manageSocket()
-    {
-        InputStream inputStream;
-
-        try
+        @Override
+        public void run()
         {
-            log.append("Getting socket input stream\n");
-            inputStream = mmSocket.getInputStream();
-            byte[] buffer = new byte[1024];
-
-            log.append("Getting data file size\n");
-            inputStream.read(buffer, 0, 4);
-            int size = ByteBuffer.wrap(buffer, 0, 4).getInt();
-            int read;
-
-            log.append("Receiving data file\n");
-            File f = File.createTempFile("data", null);
-            FileOutputStream ofstream = new FileOutputStream(f);
-            while((read = inputStream.read(buffer)) > 0)
+            bluetoothAdapter.cancelDiscovery();
+            try
             {
-                ofstream.write(buffer, 0, read);
+                log.append("Attempting to connect to Pi.\n");
+
+                mmSocket.connect();
             }
-            ofstream.close();
-
-            TableLayout tl = view.findViewById(R.id.dataTable);
-            tl.removeAllViews(); //clear the table
-
-            String[] headings = {"Time", "pH", "EC", "Temperature"};
-            addTableRow(tl, headings);
-
-            String line;
-            BufferedReader reader = new BufferedReader(new FileReader(f));
-            while((line = reader.readLine()) != null)
+            catch(IOException connectException)
             {
-                addTableRow(tl, line.split(","));
+                log.append("Failed to connect to Pi: ");
+                log.append(String.format("%s\n", connectException.toString()));
+                try
+                {
+                    mmSocket.close();
+                }
+                catch (IOException ignored) { }
+
+                return;
             }
 
-            f.deleteOnExit();
+            // Do whatever needs to be done with mmSocket
+            manageSocket();
         }
-        catch(Exception ignored) {}
 
-        log.append("Done.\n");
-        cancel();
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException ignored) {
+            }
+        }
+
+        public void manageSocket()
+        {
+            InputStream inputStream;
+            OutputStream outputStream;
+
+            try
+            {
+                log.append("Getting socket input stream\n");
+                inputStream = mmSocket.getInputStream();
+                outputStream = mmSocket.getOutputStream();
+                byte[] buffer = new byte[1024];
+
+                buffer[0] = 0;
+                outputStream.write(buffer, 0, 1);
+
+                log.append("Getting data file size\n");
+                inputStream.read(buffer, 0, 4);
+                ByteBuffer byteBuffer = ByteBuffer.wrap(buffer, 0, 4);
+                byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
+                int size = byteBuffer.getInt();
+                int read, remaining;
+                remaining = size;
+
+                log.append("Receiving data file\n");
+                final File f = File.createTempFile("data", null);
+                FileOutputStream ofstream = new FileOutputStream(f);
+                while(remaining > 0)
+                {
+                    read = inputStream.read(buffer);
+                    ofstream.write(buffer, 0, read);
+                    remaining -= read;
+                }
+                ofstream.close();
+
+                outputStream.write(buffer, 0, 1);
+
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TableLayout tl = view.findViewById(R.id.dataTable);
+                        tl.removeAllViews(); //clear the table
+
+                        String[] headings = {"Time", "pH", "EC", "Temperature", "CO2"};
+                        addTableRow(tl, headings);
+
+                        try
+                        {
+                            String line;
+                            BufferedReader reader = new BufferedReader(new FileReader(f));
+                            while((line = reader.readLine()) != null)
+                            {
+                                addTableRow(tl, line.split(","));
+                                //log.append(line + "\n");
+                            }
+
+                            f.deleteOnExit();
+                        }
+                        catch (Exception ignored) { }
+                    }
+                });
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+
+            log.append("Done.\n");
+            cancel();
+        }
     }
-}
 }
